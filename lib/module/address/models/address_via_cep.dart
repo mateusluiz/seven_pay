be319b9ac1e564c77e725893e272ba7653a27975
Mateus Luiz <mateus74890@gmail.com>
2024-05-25 00:04:27 -0300

    feat: Create address model

---
+++ lib/module/address/models/address_via_cep.dart
@@ -0,0 +1,31 @@
+class Address {
+  final String cep;
+  final String logradouro;
+  final String complemento;
+  final String bairro;
+  final String localidade;
+  final String uf;
+  final String ibge;
+
+  Address({
+    required this.cep,
+    required this.logradouro,
+    required this.complemento,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+    required this.ibge,
+  });
+
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return Address(
+      cep: json['cep'],
+      logradouro: json['logradouro'],
+      complemento: json['complemento'],
+      bairro: json['bairro'],
+      localidade: json['localidade'],
+      uf: json['uf'],
+      ibge: json['ibge'],
+    );
+  }
+}
